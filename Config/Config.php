52ed71b4ce4462fefddaf242837d5f766fd5599a
Daniel Zarategui <66543074+DanieruZ@users.noreply.github.com>
2022-10-16 22:53:25 -0300

    add template

---
+++ Config/Config.php
@@ -0,0 +1,18 @@
+<?php namespace Config;
+
+define("ROOT", dirname(__DIR__) . "/");
+//Path to your project's root folder
+define("FRONT_ROOT", "/projects/pet_hero/");
+define("VIEWS_PATH", "Views/");
+define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
+define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "pets");
+define("DB_USER", "root");
+define("DB_PASS", "");
+?>
+
+
+
+
---
+++ Views/login.php
@@ -0,0 +1,14 @@
+<main class="d-flex align-items-center justify-content-center height-100" >
+<div class="content">
+  <header class="text-center">
+    <h2>Pet Hero</h2>
+  </header>
+  <form action="Login/login" method="post" class="login-form bg-dark-alpha p-5 bg-light">
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="email" id="email" name="email" class="form-control form-control-lg" placeholder="Ingresar email">
+    </div>
+    <button class="btn btn-dark btn-block btn-lg" type="submit">Iniciar Sesión</button>
+  </form>
+</div>
+</main>
